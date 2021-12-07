7069264160f0f301aa495d87c9e8eea4c3e05240
Alexander Woestman <awoestmann@intevation.de>
2021-12-07 13:55:20 +0100

    Add script and resources to configure the Keycloak after initial launch
    The script adds a realm for the iam application and necessary clients.

--- docker/keycloak/README.md
+++ docker/keycloak/README.md
@@ -17,7 +17,7 @@ docker build -t imis3/iam_keycloak .
 Run container
 
 ```bash
-docker run --name imis3_iam_keycloak --network imis3 -p 48080:8080 -d imis3/iam_keycloak
+docker run --name imis3_iam_keycloak -v $PWD:/opt/src --network imis3 -p 48080:8080 -d imis3/iam_keycloak
 ```
 
 The Keycloak admin ui can then be reached under localhost:48080. An initial user is created during the setup:
@@ -26,3 +26,13 @@ The Keycloak admin ui can then be reached under localhost:48080. An initial user
 * Password: secret
 
 These credentials can be used to log in into the web interface and further configure the application.
+
+After starting the container the first time, the configure.sh script can be used to do the initial Keycloak configuration:
+
+```bash
+docker exec -ti imis3_iam_keycloak bash
+cd /opt/src
+./configure.sh
+```
+
+This will create a new realm for the imis3 iam application and the neccessary clients.
---
+++ docker/keycloak/configure.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Script for the inital keycloak setup:
+# - Import a realm for the imis applications
+# - Create an openid client with enabled service accounts to access the admin api without user credentials
+# - Create a saml client for the iam web application
+
+
+KEYCLOAK_URL=http://localhost:8080/auth
+KEYCLOAK_REALM=master
+KEYCLOAK_CLIENT_ID=clientId
+KEYCLOAK_CLIENT_SECRET=clientSecret
+KEYCLOAK_ADMIN_USER=admin
+KEYCLOAK_ADMIN_PW=secret
+IMIS_REALM=imis3
+
+#Get access token
+TKN=$(curl -X POST "${KEYCLOAK_URL}/realms/${KEYCLOAK_REALM}/protocol/openid-connect/token" \
+ -H "Content-Type: application/x-www-form-urlencoded" \
+ -d "username=${KEYCLOAK_ADMIN_USER}" \
+ -d "password=${KEYCLOAK_ADMIN_PW}" \
+ -d 'grant_type=password' \
+ -d 'client_id=admin-cli' | jq -r '.access_token')
+
+#Import imis3 realm
+REALM_CONFIG=$(sed "s/%IMIS_REALM/${IMIS_REALM}/g" realm_config.json)
+echo "Creating imis realm '$IMIS_REALM'"
+curl -X POST "${KEYCLOAK_URL}/admin/realms" \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $TKN" \
+    --data "$REALM_CONFIG"
+
+#Create openid service client for service account access
+echo "Creating service client: $IMIS_REALM->$(jq .clientId service_client.json)"
+curl -X POST "${KEYCLOAK_URL}/admin/realms/$IMIS_REALM/clients" \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $TKN" \
+    -d @service_client.json
+
+#Create saml client for the client web application
+echo "Creating iam client: $IMIS_REALM->$(jq .clientId iam_client.json)"
+curl -X POST "${KEYCLOAK_URL}/admin/realms/$IMIS_REALM/clients" \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $TKN" \
+    -d @iam_client.json
