aac24d62387367624024ce9735ba73b5e2e805a2
Tom Gottfried <Tom Gottfried tom@intevation.de>
2022-01-21 18:08:47 +0100

    Use PostgreSQL database with Keycloak
    Since Keycloak uses liquibase for database schema management, we have to keep
    the database privileges lax for the schema, because liquibase needs to be able
    to create tables just for not making the server startup fail, even if we
    would configure manual initialization and updates of the schema (!).
    
    There can still be timing problems when starting with docker-compose because
    Keycloak just exits if it does not immediately get a database connection,
    e.g. because the database is still starting up. A more healthy behavior would
    be to retry to connect to the database.
    
    Configuration of realm, client and example user is now done during startup
    of Keycloak, because it requires the database to be available, which is no
    more the case during image build.

--- docker/keycloak/configure.sh
+++ docker/keycloak/configure.sh
@@ -13,26 +13,9 @@ then
 fi
 JBOSS_CLI=$KEYCLOAK_HOME/bin/jboss-cli.sh
 
-KEYCLOAK_URL=http://localhost:8080/auth
-KEYCLOAK_REALM=master
-KEYCLOAK_CLIENT_ID=clientId
-KEYCLOAK_CLIENT_SECRET=clientSecret
 KEYCLOAK_ADMIN_USER=admin
 KEYCLOAK_ADMIN_PW=secret
-IMIS_REALM=imis3
 
-function wait_for_server() {
-    wait=10
-    waited=0
-    until $JBOSS_CLI -c "ls /deployment"; do
-        sleep 1
-        waited=$(($waited + 1))
-        if [ $waited -gt $wait ]; then
-            echo "Failed starting Keycloak"
-            exit 1
-        fi
-    done
-}
 
 # Add admin users for container and keycloak web application
 ${KEYCLOAK_HOME}/bin/add-user.sh --silent $KEYCLOAK_ADMIN_USER $KEYCLOAK_ADMIN_PW
@@ -41,49 +24,3 @@ ${KEYCLOAK_HOME}/bin/add-user-keycloak.sh -r master \
 
 # Configure PostgreSQL JDBC driver and data source
 $JBOSS_CLI --file=${DIR}/commands.cli
-
-# Start server
-${KEYCLOAK_HOME}/bin/standalone.sh &
-wait_for_server
-
-#Get access token
-TKN=$(curl -sX POST "${KEYCLOAK_URL}/realms/${KEYCLOAK_REALM}/protocol/openid-connect/token" \
- -H "Content-Type: application/x-www-form-urlencoded" \
- -d "username=${KEYCLOAK_ADMIN_USER}" \
- -d "password=${KEYCLOAK_ADMIN_PW}" \
- -d 'grant_type=password' \
- -d 'client_id=admin-cli' | jq -r '.access_token')
-
-#Import imis3 realm
-REALM_CONFIG=$(sed "s/%IMIS_REALM/${IMIS_REALM}/g" ${DIR}/realm_config.json)
-echo "Creating imis realm '$IMIS_REALM'"
-curl -sX POST "${KEYCLOAK_URL}/admin/realms" \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Bearer $TKN" \
-    --data "$REALM_CONFIG"
-
-#Create openid service client for service account access
-echo "Creating service client: $IMIS_REALM->$(jq .clientId ${DIR}/service_client.json)"
-curl -sX POST "${KEYCLOAK_URL}/admin/realms/$IMIS_REALM/clients" \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Bearer $TKN" \
-    -d @${DIR}/service_client.json
-
-#Create saml client for the client web application
-echo "Creating iam client: $IMIS_REALM->$(jq .clientId ${DIR}/iam_client.json)"
-curl -sX POST "${KEYCLOAK_URL}/admin/realms/$IMIS_REALM/clients" \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Bearer $TKN" \
-    -d @${DIR}/iam_client.json
-
-#Add inital user
-KEYCLOAK_IAM_USER=$(jq .username ${DIR}/iam_user.json)
-KEYCLOAK_IAM_PW=$(jq '.credentials[0].value' ${DIR}/iam_user.json)
-echo "Creating iam user: $IMIS_REALM->$KEYCLOAK_IAM_USER - $KEYCLOAK_IAM_PW"
-curl -sX POST "${KEYCLOAK_URL}/admin/realms/$IMIS_REALM/users" \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Bearer $TKN" \
-    -d @${DIR}/iam_user.json
-
-# Stop server
-$JBOSS_CLI -c ":shutdown"
--- docker/keycloak/start.sh
+++ docker/keycloak/start.sh
@@ -1,3 +1,68 @@
-#!/bin/sh
+#!/bin/sh -e
 
-${KEYCLOAK_HOME}/bin/standalone.sh -b 0.0.0.0 -bmanagement=0.0.0.0
+DIR=$(dirname $0)
+
+if [ -z $KEYCLOAK_HOME ]
+then
+    KEYCLOAK_HOME=/opt/keycloak
+fi
+
+wait_for_server () {
+    wait=10
+    waited=0
+    until ${KEYCLOAK_HOME}/bin/jboss-cli.sh -c "ls /deployment"; do
+        sleep 1
+        waited=$(($waited + 1))
+        if [ $waited -gt $wait ]; then
+            echo "Failed starting Keycloak"
+            exit 1
+        fi
+    done
+}
+
+# Start Keycloak
+${KEYCLOAK_HOME}/bin/standalone.sh -b 0.0.0.0 -bmanagement=0.0.0.0 &
+wait_for_server
+
+echo "Configure realm, SAML client and example account if not existing ..."
+KEYCLOAK_URL=http://localhost:8080/auth
+KEYCLOAK_REALM=master
+KEYCLOAK_ADMIN_USER=admin
+KEYCLOAK_ADMIN_PW=secret
+IMIS_REALM=imis3
+IMIS_CLIENT=iam-client
+
+# Get access token
+TKN=$(curl -sX POST "${KEYCLOAK_URL}/realms/${KEYCLOAK_REALM}/protocol/openid-connect/token" \
+ -H "Content-Type: application/x-www-form-urlencoded" \
+ -d "username=${KEYCLOAK_ADMIN_USER}" \
+ -d "password=${KEYCLOAK_ADMIN_PW}" \
+ -d 'grant_type=password' \
+ -d 'client_id=admin-cli' | jq -r '.access_token')
+
+# Import imis3 realm
+REALM_CONFIG=$(sed "s/%IMIS_REALM/${IMIS_REALM}/g" ${DIR}/realm_config.json)
+echo "Creating imis realm '$IMIS_REALM'"
+curl -sX POST "${KEYCLOAK_URL}/admin/realms" \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $TKN" \
+    --data "$REALM_CONFIG"
+
+# Create SAML client for the client web application
+echo "Creating iam client: $IMIS_REALM->$(jq .clientId ${DIR}/iam_client.json)"
+curl -sX POST "${KEYCLOAK_URL}/admin/realms/$IMIS_REALM/clients" \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $TKN" \
+    -d @${DIR}/iam_client.json
+
+# Add inital user
+KEYCLOAK_IAM_USER=$(jq .username ${DIR}/iam_user.json)
+KEYCLOAK_IAM_PW=$(jq '.credentials[0].value' ${DIR}/iam_user.json)
+echo "Creating iam user: $IMIS_REALM->$KEYCLOAK_IAM_USER - $KEYCLOAK_IAM_PW"
+curl -sX POST "${KEYCLOAK_URL}/admin/realms/$IMIS_REALM/users" \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $TKN" \
+    -d @${DIR}/iam_user.json
+
+echo "... done"
+wait $!
