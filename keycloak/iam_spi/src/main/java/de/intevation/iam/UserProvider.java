496bd095a1d0796dc4f53dee203a05de4d7ae524
Alexander Woestman <awoestmann@intevation.de>
2022-04-29 10:46:30 +0200

    Add first spi implementation
    The spi should be compiled and deployed automatically during the docker compose setup.
    It currently provides an interface that returns the profile data of the current user.

---
+++ keycloak/iam_spi/src/main/java/de/intevation/iam/UserProvider.java
@@ -0,0 +1,61 @@
+package de.intevation.iam;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.UserModel;
+import org.keycloak.services.managers.AppAuthManager;
+import org.keycloak.services.managers.AuthenticationManager.AuthResult;
+import org.keycloak.services.resource.RealmResourceProvider;
+
+public class UserProvider implements RealmResourceProvider {
+
+    private KeycloakSession session;
+    private AuthResult auth;
+
+    public UserProvider(KeycloakSession session) {
+        this.session = session;
+        this.auth = new AppAuthManager.BearerTokenAuthenticator(this.session).authenticate();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/profile")
+    public Response getProfile(
+            @Context UriInfo info,
+            @Context HttpServletRequest request) {
+        UserModel user = auth.getUser();
+        return Response.ok(userToJson(user)).build();
+    }
+
+    @Override
+    public void close() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Object getResource() {
+        return this;
+    }
+
+    private JsonObject userToJson(UserModel user) {
+        JsonObjectBuilder builder = Json.createObjectBuilder();
+        builder.add("id", user.getId());
+        builder.add("firstname", user.getFirstName());
+        builder.add("lastname", user.getLastName());
+        builder.add("mail", user.getEmail());
+        builder.add("username", user.getUsername());
+        return builder.build();
+    }
+}
---
+++ keycloak/iam_spi/src/main/java/de/intevation/iam/UserProviderFactory.java
@@ -0,0 +1,39 @@
+package de.intevation.iam;
+
+import org.keycloak.Config.Scope;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.services.resource.RealmResourceProvider;
+import org.keycloak.services.resource.RealmResourceProviderFactory;
+
+/**
+ * RealmResourceProviderFactory used to create the UserUrlProvider
+ * 
+ * @author Alexander Woestmann <awoestmann@intevation.de>
+ */
+public class UserProviderFactory implements RealmResourceProviderFactory {
+
+    public static final String ID = "iamuser";
+
+    @Override
+    public String getId() {
+        return ID;
+    }
+
+    @Override
+    public RealmResourceProvider create(KeycloakSession session) {
+        return new UserProvider(session);
+    }
+
+    @Override
+    public void init(Scope config) {
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory factory) {
+    }
+
+    @Override
+    public void close() {
+    }
+}
