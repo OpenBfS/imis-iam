62f4c4a4bd380d0279485d54c321fd59f2529f71
Alexander Woestman <awoestmann@intevation.de>
2022-05-04 13:54:35 +0200

    Update spi profile interface to properly allow reading/writing ones profile
    Both methods should be reachable by using GET/PUT requests to /realms/{realm}/iamuser/profile

--- keycloak/iam_spi/src/main/java/de/intevation/iam/UserProvider.java
+++ keycloak/iam_spi/src/main/java/de/intevation/iam/UserProvider.java
@@ -4,15 +4,22 @@ import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.UserModel;
+import org.keycloak.representations.account.UserRepresentation;
 import org.keycloak.services.managers.AppAuthManager;
 import org.keycloak.services.managers.AuthenticationManager.AuthResult;
 import org.keycloak.services.resource.RealmResourceProvider;
+import org.keycloak.userprofile.UserProfile;
+import org.keycloak.userprofile.UserProfileContext;
+import org.keycloak.userprofile.UserProfileProvider;
 
 public class UserProvider implements RealmResourceProvider {
 
@@ -21,15 +28,34 @@ public class UserProvider implements RealmResourceProvider {
 
     public UserProvider(KeycloakSession session) {
         this.session = session;
-        this.auth = new AppAuthManager.BearerTokenAuthenticator(this.session).authenticate();
+        this.auth = new AppAuthManager().authenticateIdentityCookie(session, session.getContext().getRealm());
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    // @Path("/profile")
+    @Path("/profile")
     public Response getProfile() {
         UserModel user = auth.getUser();
-        return Response.ok(userToJson(user)).build();
+        if (user == null) {
+            return Response.status(Status.FORBIDDEN).build();
+        }
+        return Response.ok(userToJson(user).toString()).build();
+    }
+
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/profile")
+    public Response updateProfile(
+        final UserRepresentation rep
+    ) {
+        UserModel user = auth.getUser();
+        if (!user.getId().equals(rep.getId())) {
+            return Response.status(Status.FORBIDDEN).build();
+        }
+        UserProfileProvider profileProvider = session.getProvider(UserProfileProvider.class);
+        UserProfile profile = profileProvider.create(UserProfileContext.USER_API, rep.toAttributes());
+        profile.update(false);
+        return Response.ok(userToJson(user).toString()).build();
     }
 
     @Override
@@ -41,7 +67,6 @@ public class UserProvider implements RealmResourceProvider {
     public Object getResource() {
         return this;
     }
-
     private JsonObject userToJson(UserModel user) {
         JsonObjectBuilder builder = Json.createObjectBuilder();
         builder.add("id", user.getId());
