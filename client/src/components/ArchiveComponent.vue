f1581a71431433f34f0d536c6189334557565539
Alexander Woestman <awoestmann@intevation.de>
2022-05-18 10:57:52 +0200

    Add placeholder content to archive component

--- client/src/components/ArchiveComponent.vue
+++ client/src/components/ArchiveComponent.vue
@@ -1,10 +1,37 @@
 <template>
   <v-card class="ma-2 pa-2">
-    <v-card-title>Archive</v-card-title>
+    <v-container>
+      <v-row>
+        <v-col cols="3">
+          <v-card-title>Archive</v-card-title>
+          <li v-for="date in dates" :key="date.year">
+            {{ date.year }}
+          </li>
+        </v-col>
+        <v-col align="left" justify="left">
+          <v-btn>Search</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-card>
 </template>
 <script>
 export default {
-  setup() {},
+  setup() {
+    const dates = [
+      {
+        year: 2022,
+      },
+      {
+        year: 2021,
+      },
+      {
+        year: 2020,
+      },
+    ];
+    return {
+      dates,
+    };
+  },
 };
 </script>
