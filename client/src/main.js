59bde8e9c159a944f459047aca70c9fe0a259e95
Alexander Woestman <awoestmann@intevation.de>
2022-06-09 16:26:44 +0200

    Add vue-i18n to client

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,27 @@
 import { createApp } from "vue";
+import { createI18n, useI18n } from "vue-i18n";
+import { createVueI18nAdapter } from "vuetify/locale/adapters/vue-i18n";
+import { loadFonts } from "./plugins/webfontloader";
+
+import vuetify from "./plugins/vuetify";
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import vuetify from "./plugins/vuetify";
-import { loadFonts } from "./plugins/webfontloader";
+
+import de from "./locales/de";
 
 loadFonts();
 
-createApp(App).use(router).use(store).use(vuetify).mount("#app");
+const i18n = createI18n({
+  legacy: false,
+  globalInjection: true,
+  locale: "de",
+  messages: {
+    de: de,
+  },
+});
+
+vuetify.locale = createVueI18nAdapter({ i18n, useI18n });
+
+createApp(App).use(router).use(store).use(i18n).use(vuetify).mount("#app");
