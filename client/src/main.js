c9cb0d43344955ac363ad5181d8159ddd1f52113
Fadi Abbud <fadi.abbud@intevation.de>
2022-06-14 08:34:07 +0200

    Add "Roboto" fonts as dependency
    * instead of loading them from the internet.
    * Adjust the main.js and vuetify.js files according to this.
    * Remove "webfontloader" dependency as these is not required any more.

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import { createI18n, useI18n } from "vue-i18n";
 import { createVueI18nAdapter } from "vuetify/locale/adapters/vue-i18n";
-import { loadFonts } from "./plugins/webfontloader";
 
 import vuetify from "./plugins/vuetify";
 
@@ -10,8 +9,6 @@ import router from "./router";
 import store from "./store";
 
 import de from "./locales/de";
-
-loadFonts();
 
 const i18n = createI18n({
   legacy: false,
