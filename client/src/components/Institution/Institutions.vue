c34dcc9f6afddd6801a186d9f439108adc8b337b
Fadi Abbud <fadi.abbud@intevation.de>
2022-08-09 11:50:06 +0200

    Add license headers to institutions files

--- client/src/components/Institution/Institutions.vue
+++ client/src/components/Institution/Institutions.vue
@@ -1,3 +1,11 @@
+<!--
+/* Copyright (C) 2022 by Bundesamt fuer Strahlenschutz
+ * Software engineering by Intevation GmbH
+ *
+ * This file is Free Software under the GNU GPL (v>=3)
+ * and comes with ABSOLUTELY NO WARRANTY!
+ */
+ -->
 <template>
   <v-container>
     <v-row>
@@ -95,13 +103,6 @@
 </template>
 
 <script>
-/* Copyright (C) 2022 by Bundesamt fuer Strahlenschutz
- * Software engineering by Intevation GmbH
- *
- * This file is Free Software under the GNU GPL (v>=3)
- * and comes with ABSOLUTELY NO WARRANTY!
- */
-
 import { computed, onMounted, ref, defineAsyncComponent } from "vue";
 import { useStore } from "vuex";
 import { useNotification } from "@/lib/use-notification";
--- client/src/components/Institution/ManageInstitution.vue
+++ client/src/components/Institution/ManageInstitution.vue
@@ -1,3 +1,11 @@
+<!--
+/* Copyright (C) 2022 by Bundesamt fuer Strahlenschutz
+ * Software engineering by Intevation GmbH
+ *
+ * This file is Free Software under the GNU GPL (v>=3)
+ * and comes with ABSOLUTELY NO WARRANTY!
+ */
+ -->
 <template>
   <v-dialog v-model="show">
     <v-card width="80vw" v-if="['add', 'edit'].indexOf(processType) !== -1">
