585adce016b0c6c4ce0be187fdc8ee695af5ce92
Fadi Abbud <fadi.abbud@intevation.de>
2022-08-25 14:39:26 +0200

    Implement validating the URL-parameters for lists-export request
     * Validate each csv-options against the accepted predefined values.

--- keycloak/iam_spi/src/main/java/de/intevation/iam/ExportProvider.java
+++ keycloak/iam_spi/src/main/java/de/intevation/iam/ExportProvider.java
@@ -9,6 +9,7 @@ package de.intevation.iam;
 import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -22,6 +23,7 @@ import org.keycloak.services.resource.RealmResourceProvider;
 import de.intevation.iam.model.Institution;
 import de.intevation.iam.model.User;
 import de.intevation.iam.util.CSVExporter;
+import javax.ws.rs.core.Response.Status;
 
 public class ExportProvider implements RealmResourceProvider {
 
@@ -43,8 +45,44 @@ public class ExportProvider implements RealmResourceProvider {
         }
     }
 
+    private Boolean isAccepted(String name, CsvOptions options[]) {
+        for (CsvOptions opt : options) {
+            try {
+                if (opt.equals(CsvOptions.valueOf(name))) {
+                    return true;
+                }
+            } catch (IllegalArgumentException iae) {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    private Boolean isValidFieldSeperator(String fieldSeparator) {
+        CsvOptions[] fieldSperators = { CsvOptions.comma,
+                CsvOptions.period, CsvOptions.semicolon, CsvOptions.space };
+        return isAccepted(fieldSeparator, fieldSperators);
+    }
+
+    private Boolean isValidQuoteType(String quoteType) {
+        CsvOptions[] quoteTypes = { CsvOptions.singlequote,
+                CsvOptions.doublequote };
+        return isAccepted(quoteType, quoteTypes);
+    }
+
+    private Boolean isValidRowDelimiter(String rowDelimiter) {
+        CsvOptions[] rowFelimiters = { CsvOptions.windows,
+                CsvOptions.linux };
+        return isAccepted(rowDelimiter, rowFelimiters);
+    }
+
+    private Boolean isValidEncoding(String encoding) {
+        return Arrays.asList("utf-8", "utf-16", "ascii").contains(encoding);
+    }
+
     /**
      * Constructor.
+     *
      * @param session
      */
     public ExportProvider(KeycloakSession session) {
@@ -53,91 +91,130 @@ public class ExportProvider implements RealmResourceProvider {
 
     /**
      * Export users to csv.
+     *
      * @param fieldSeparator Char used as field separator
-     * @param quoteType Char used for quotes
-     * @param rowDelimiter Char used as row delimiter
-     * @param encoding Encoding to use
+     * @param quoteType      Char used for quotes
+     * @param rowDelimiter   Char used as row delimiter
+     * @param encoding       Encoding to use
      * @return Resulting csv data
      */
     @GET
     @Path("/user")
     public Response exportUsers(
-        @QueryParam("fieldSeparator") String fieldSeparator,
-        @QueryParam("quoteType") String quoteType,
-        @QueryParam("rowDelimiter") String rowDelimiter,
-        @QueryParam("encoding") String encoding
-    ) {
+            @QueryParam("fieldSeparator") String fieldSeparator,
+            @QueryParam("quoteType") String quoteType,
+            @QueryParam("rowDelimiter") String rowDelimiter,
+            @QueryParam("encoding") String encoding) {
         CSVExporter<User> exporter = new CSVExporter<User>();
         if (fieldSeparator != null) {
+            if (!isValidFieldSeperator(fieldSeparator)) {
+                return Response.status(Status.BAD_REQUEST)
+                        .type(MediaType.APPLICATION_JSON)
+                        .build();
+            }
             exporter.setFieldSeparator(
                     CsvOptions.valueOf(fieldSeparator).getChar());
         }
         if (quoteType != null) {
+            if (!isValidQuoteType(quoteType)) {
+                return Response.status(Status.BAD_REQUEST)
+                        .type(MediaType.APPLICATION_JSON)
+                        .build();
+            }
             exporter.setQuoteType(
                     CsvOptions.valueOf(quoteType).getChar());
         }
         if (rowDelimiter != null) {
+            if (!isValidRowDelimiter(rowDelimiter)) {
+                return Response.status(Status.BAD_REQUEST)
+                        .type(MediaType.APPLICATION_JSON)
+                        .build();
+            }
             exporter.setRowDelimiter(
                     CsvOptions.valueOf(rowDelimiter).getChar());
         }
         if (encoding != null) {
+            if (!isValidEncoding(encoding)) {
+                return Response.status(Status.BAD_REQUEST)
+                        .type(MediaType.APPLICATION_JSON)
+                        .build();
+            }
             Charset charset = Charset.forName(encoding);
             exporter.setEncoding(charset);
         }
 
         UserProvider userProvider = new UserProvider(session);
         Response userResponse = userProvider.getUsers();
-        ArrayList<User> users
-            = userResponse.readEntity(ArrayList.class);
+        ArrayList<User> users = userResponse.readEntity(ArrayList.class);
         InputStream result = exporter.export(users);
         return Response.ok(result, MediaType.APPLICATION_OCTET_STREAM)
-            .header("Content-Disposition",
-                "attachment; filename=\"export.csv\"")
-            .build();
+                .header("Content-Disposition",
+                        "attachment; filename=\"export.csv\"")
+                .build();
     }
 
     /**
      * Export institutions as csv.
+     *
      * @param fieldSeparator Char used as field separator
-     * @param quoteType Char used for quotes
-     * @param rowDelimiter Char used as row delimiter
-     * @param encoding Encoding to use
+     * @param quoteType      Char used for quotes
+     * @param rowDelimiter   Char used as row delimiter
+     * @param encoding       Encoding to use
      * @return Institutions as CSV
      */
     @GET
     @Path("/institution")
     public Response exportInstitutions(
-        @QueryParam("fieldSeparator") String fieldSeparator,
-        @QueryParam("quoteType") String quoteType,
-        @QueryParam("rowDelimiter") String rowDelimiter,
-        @QueryParam("encoding") String encoding) {
+            @QueryParam("fieldSeparator") String fieldSeparator,
+            @QueryParam("quoteType") String quoteType,
+            @QueryParam("rowDelimiter") String rowDelimiter,
+            @QueryParam("encoding") String encoding) {
         CSVExporter<Institution> exporter = new CSVExporter<Institution>();
         if (fieldSeparator != null) {
+            if (!isValidFieldSeperator(fieldSeparator)) {
+                return Response.status(Status.BAD_REQUEST)
+                        .type(MediaType.APPLICATION_JSON)
+                        .build();
+            }
             exporter.setFieldSeparator(
                     CsvOptions.valueOf(fieldSeparator).getChar());
         }
         if (quoteType != null) {
+            if (!isValidQuoteType(quoteType)) {
+                return Response.status(Status.BAD_REQUEST)
+                        .type(MediaType.APPLICATION_JSON)
+                        .build();
+            }
             exporter.setQuoteType(
                     CsvOptions.valueOf(quoteType).getChar());
         }
         if (rowDelimiter != null) {
+            if (!isValidRowDelimiter(rowDelimiter)) {
+                return Response.status(Status.BAD_REQUEST)
+                        .type(MediaType.APPLICATION_JSON)
+                        .build();
+            }
             exporter.setRowDelimiter(
                     CsvOptions.valueOf(rowDelimiter).getChar());
         }
         if (encoding != null) {
+            if (!isValidEncoding(encoding)) {
+                return Response.status(Status.BAD_REQUEST)
+                        .type(MediaType.APPLICATION_JSON)
+                        .build();
+            }
             Charset charset = Charset.forName(encoding);
             exporter.setEncoding(charset);
         }
 
         InstitutionProvider instProvider = new InstitutionProvider(session);
         Response instResponse = instProvider.getInstitutions();
-        ArrayList<Institution> institutions
-            = instResponse.readEntity(ArrayList.class);
+        ArrayList<Institution> institutions = instResponse.readEntity(ArrayList.class);
         InputStream result = exporter.export(institutions);
         return Response.ok(result, MediaType.APPLICATION_OCTET_STREAM)
-            .header("Content-Disposition",
-                    "attachment; filename=\"export.csv\"")
-            .build();
+                .header("Content-Disposition",
+                        "attachment; filename=\"export.csv\"")
+                .build();
     }
 
     @Override
