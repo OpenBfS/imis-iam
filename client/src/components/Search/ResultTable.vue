5b35bb4b5af9f655c114ee485c9bb24fc6067eba
Fadi Abbud <fadi.abbud@intevation.de>
2022-09-09 12:44:21 +0200

    Remove needless code

--- client/src/components/Search/ResultTable.vue
+++ client/src/components/Search/ResultTable.vue
@@ -27,8 +27,8 @@
 </template>
 
 <script>
-import { computed, defineAsyncComponent } from "vue";
-import { useStore } from "vuex";
+import { defineAsyncComponent } from "vue";
+
 export default {
   components: {
     InstitutionTable: defineAsyncComponent(() =>
@@ -41,32 +41,6 @@ export default {
   props: {
     type: String,
     default: () => "users",
-  },
-  setup(props) {
-    const store = useStore();
-    const items = computed(() => {
-      if (props.type === "users") {
-        return store.state.user.foundUsers;
-      } else {
-        return store.state.institution.foundInstitutions;
-      }
-    });
-    const cloneObject = (obj) => {
-      return JSON.parse(JSON.stringify(obj));
-    };
-    const selectItem = (item) => {
-      store.commit("application/setManagedItem", cloneObject(item));
-      store.commit("application/setProcessType", "edit");
-      if (props.type == "users") {
-        store.commit("application/setShowManageUserDialog", true);
-      } else {
-        store.commit("application/setShowManageInstitutionDialog", true);
-      }
-    };
-    return {
-      items,
-      selectItem,
-    };
   },
 };
 </script>
