6c5b49885d29003b7fb9ce04a15b804381243ffb
Tom Gottfried <Tom Gottfried tom@intevation.de>
2022-10-20 17:02:11 +0200

    Some whitespace to improve readability

--- keycloak/iam_spi/src/main/java/de/intevation/iam/model/jpa/UserIamAttributes.java
+++ keycloak/iam_spi/src/main/java/de/intevation/iam/model/jpa/UserIamAttributes.java
@@ -27,33 +27,45 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 public class UserIamAttributes {
     @Id
     private String id;
+
     @Column(name = "title")
     private String title;
+
     @Column(name = "phone", nullable = false)
     private String phone;
+
     @Column(name = "mobile")
     private String mobile;
+
     @Column(name = "fax")
     private String fax;
+
     @Column(name = "oe")
     private String oe;
+
     @Column(name = "bfs_location")
     private String bfsLocation;
+
     @Column(name = "position_id", nullable = false)
     private Integer position;
+
     @Column(name = "expiry_date", nullable = false)
     @JsonIgnore
     private Timestamp expiryDate;
+
     @Column(name = "inactivity_notification_sent")
     @JsonIgnore
     private Boolean inactivityNotificationSent;
+
     @Column(name = "expired_notification_sent")
     @JsonIgnore
     private Boolean expiredNotificationSent;
+
     @OneToOne
     @JoinColumn(name = "id", referencedColumnName = "id")
     @JsonIgnore
     private UserEntity userEntity;
+
     @ManyToMany
     @JoinTable(
         name = "institution_user",
@@ -65,79 +77,104 @@ public class UserIamAttributes {
     public String getId() {
         return id;
     }
+
     public void setId(String id) {
         this.id = id;
     }
+
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String title) {
         this.title = title;
     }
+
     public String getPhone() {
         return phone;
     }
+
     public void setPhone(String phone) {
         this.phone = phone;
     }
+
     public String getMobile() {
         return mobile;
     }
+
     public void setMobile(String mobile) {
         this.mobile = mobile;
     }
+
     public String getFax() {
         return fax;
     }
+
     public void setFax(String fax) {
         this.fax = fax;
     }
+
     public String getOe() {
         return oe;
     }
+
     public void setOe(String oe) {
         this.oe = oe;
     }
+
     public String getBfsLocation() {
         return bfsLocation;
     }
+
     public void setBfsLocation(String bfsLocation) {
         this.bfsLocation = bfsLocation;
     }
+
     public Integer getPosition() {
         return position;
     }
+
     public void setPosition(Integer position) {
         this.position = position;
     }
+
     public Timestamp getExpiryDate() {
         return expiryDate;
     }
+
     public void setExpiryDate(Timestamp expiryDate) {
         this.expiryDate = expiryDate;
     }
+
     public Boolean getInactivityNotificationSent() {
         return inactivityNotificationSent;
     }
+
     public void setInactivityNotificationSent(
             Boolean inactivityNotificationSent) {
         this.inactivityNotificationSent = inactivityNotificationSent;
     }
+
     public Boolean getExpiredNotificationSent() {
         return expiredNotificationSent;
     }
+
     public void setExpiredNotificationSent(Boolean expiredNotificationSent) {
         this.expiredNotificationSent = expiredNotificationSent;
     }
+
     public UserEntity getUserEntity() {
         return userEntity;
     }
+
     public void setUserEntity(UserEntity userEntity) {
         this.userEntity = userEntity;
     }
+
     public List<Institution> getInstitutions() {
         return institutions;
     }
+
     public void setInstitutions(List<Institution> institutions) {
         this.institutions = institutions;
     }
