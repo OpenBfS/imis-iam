22e6a32ca8396f22a15aea9d35b0765a01b0b47f
Tom Gottfried <Tom Gottfried tom@intevation.de>
2023-11-23 11:27:34 +0100

    Add missing headers

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,3 +1,9 @@
+/* Copyright (C) 2023 by Bundesamt fuer Strahlenschutz
+ * Software engineering by Intevation GmbH
+ *
+ * This file is Free Software under the GNU GPL (v>=3)
+ * and comes with ABSOLUTELY NO WARRANTY!
+ */
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
 };
--- client/vue.config.js
+++ client/vue.config.js
@@ -1,3 +1,9 @@
+/* Copyright (C) 2023 by Bundesamt fuer Strahlenschutz
+ * Software engineering by Intevation GmbH
+ *
+ * This file is Free Software under the GNU GPL (v>=3)
+ * and comes with ABSOLUTELY NO WARRANTY!
+ */
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
