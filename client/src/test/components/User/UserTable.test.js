cb201166fb3b7b6770270ff51040afcb833d598f
Tom Gottfried <Tom Gottfried tom@intevation.de>
2023-11-24 12:53:33 +0100

    One more test for the UserTable component

--- client/src/test/components/User/UserTable.test.js
+++ client/src/test/components/User/UserTable.test.js
@@ -44,21 +44,26 @@ const users = [
   {
     id: "2",
     username: "two",
-    firstName: "Two",
     lastName: "Three",
   },
 ];
 
+// Init component
+const wrapper = mount(UserTable, {
+  props: {
+    users: users,
+  },
+  global: {
+    plugins: [vuetify, store, i18n],
+  },
+});
+
 test("Username is displayed in first column", () => {
-  const wrapper = mount(UserTable, {
-    props: {
-      users: users,
-    },
-    global: {
-      plugins: [vuetify, store, i18n],
-    },
-  });
   wrapper.findAll("tr").forEach((row, i) => {
     expect(row.find("td").text()).toBe(users[i].username);
   });
+});
+
+test("Missing attribute is rendered as empty string", () => {
+  expect(wrapper.findAll("tr")[1].findAll("td")[1].text()).toBe("");
 });
