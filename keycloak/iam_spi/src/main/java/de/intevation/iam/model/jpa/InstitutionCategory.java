92a63f24aff12f3c52eccb6b757d6a8b73bcc601
Christoph Klassen <christoph.klassen@intevation.de>
2024-01-09 16:29:12 +0100

    purge dead code

--- keycloak/iam_spi/src/main/java/de/intevation/iam/model/jpa/InstitutionCategory.java
+++ keycloak/iam_spi/src/main/java/de/intevation/iam/model/jpa/InstitutionCategory.java
@@ -10,26 +10,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 
 @Entity
 @Table(name = "iam_institution_category", schema = "keycloak")
 public class InstitutionCategory {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-
     @Column(name = "name", nullable = false)
     private String name;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
