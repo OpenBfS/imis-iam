b91be6eab4a866a49d496217c778d761c3f2c6ed
Christoph Klassen <christoph.klassen@intevation.de>
2024-02-21 10:35:44 +0100

    Add imis_usergroup_id to example institutions

--- docker/keycloak/add_example_institutions.sql
+++ docker/keycloak/add_example_institutions.sql
@@ -1,6 +1,6 @@
-INSERT INTO keycloak.iam_institution(name, short_name, service_building_street, service_building_postal_code, service_building_location, central_phone, central_mail, imis_id) VALUES
-    ('Institution 1', 'inst_1', 'Examplestreet 1', '12345', 'ExampleLocation-1', '0123/456789', 'inst1@example.test', 'inst1'),
-    ('Institution 2', 'inst_2', 'Examplestreet 2', '22345', 'ExampleLocation-2', '0123/456789', 'inst2@example.test', 'inst2');
+INSERT INTO keycloak.iam_institution(name, short_name, service_building_street, service_building_postal_code, service_building_location, central_phone, central_mail, imis_id, imis_usergroup_id) VALUES
+    ('Institution 1', 'inst_1', 'Examplestreet 1', '12345', 'ExampleLocation-1', '0123/456789', 'inst1@example.test', 'inst1', 'ABC'),
+    ('Institution 2', 'inst_2', 'Examplestreet 2', '22345', 'ExampleLocation-2', '0123/456789', 'inst2@example.test', 'inst2', 'DEF');
 
 INSERT INTO keycloak.iam_institution_user(user_id, institution_id) VALUES
 ((SELECT id FROM keycloak.user_entity WHERE username = 'exampleuser'), (SELECT id FROM keycloak.iam_institution WHERE name = 'Institution 1')),
