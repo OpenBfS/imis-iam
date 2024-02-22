ee42c4fd9c8fb1efc0103071c20fd133e465a92e
Tom Gottfried <Tom Gottfried tom@intevation.de>
2024-02-22 11:09:14 +0100

    Add missing license header
    Done by `yarn lint'.

--- client/src/test/components/mockRouter.js
+++ client/src/test/components/mockRouter.js
@@ -1,3 +1,9 @@
+/* Copyright (C) 2024 by Bundesamt fuer Strahlenschutz
+ * Software engineering by Intevation GmbH
+ *
+ * This file is Free Software under the GNU GPL (v>=3)
+ * and comes with ABSOLUTELY NO WARRANTY!
+ */
 import { createRouter, createWebHashHistory } from "vue-router";
 import Archive from "@/components/Mailing/Archive.vue";
 
