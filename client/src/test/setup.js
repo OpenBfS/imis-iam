f7a070683f85e073f7c2821fdc358405146876a4
Tom Gottfried <Tom Gottfried tom@intevation.de>
2024-05-07 12:09:15 +0200

    Add missing copyright header

--- client/src/test/setup.js
+++ client/src/test/setup.js
@@ -1,3 +1,9 @@
+/* Copyright (C) 2024 by Bundesamt fuer Strahlenschutz
+ * Software engineering by Intevation GmbH
+ *
+ * This file is Free Software under the GNU GPL (v>=3)
+ * and comes with ABSOLUTELY NO WARRANTY!
+ */
 import { vi } from "vitest";
 
 // The v-data-table-server is leading to the error "ReferenceError: ResizeObserver is not defined".
