e9e4e2ac446315ade69ed71266fb184643716de5
Christoph Klassen <christoph.klassen@intevation.de>
2024-06-01 12:51:58 +0200

    Migrate from Vue CLI to Vite
    Vue CLI won't be developed further so we switch to the
    build tool Vite which is recommended by the team behind Vuejs.

--- client/babel.config.js
+++
@@ -1,9 +0,0 @@
-/* Copyright (C) 2023 by Bundesamt fuer Strahlenschutz
- * Software engineering by Intevation GmbH
- *
- * This file is Free Software under the GNU GPL (v>=3)
- * and comes with ABSOLUTELY NO WARRANTY!
- */
-module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"],
-};
