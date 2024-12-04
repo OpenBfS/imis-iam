9835343e7f0c326b3f0bb1c3ba91199fdc226980
Christoph Klassen <christoph.klassen@intevation.de>
2024-12-04 17:43:32 +0100

    Export port of Vite server to network
    If using localhost for Vite its port is only exposed in the client
    container but not to the network.

--- docker/client/start.sh
+++ docker/client/start.sh
@@ -5,4 +5,4 @@ service shibd restart
 
 rm -f /run/apache2/apache2.pid
 apache2ctl -D FOREGROUND &
-yarn install && yarn dev --host --port ${SP_PORT}
+yarn install && yarn dev --host ${CLIENT_HOST} --port ${SP_PORT}
