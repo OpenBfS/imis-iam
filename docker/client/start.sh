7d21ade27152f6693ff3dd7e1a9eeafc3b8124d3
Christoph Klassen <christoph.klassen@intevation.de>
2025-01-02 15:06:30 +0100

    Use env var to decide if to start dev or prod mode
    This is an easy way to allow to start one of both modes
    without having to create more docker setups.
    
    Since the start.sh script is a bash script run it with bash
    to avoid syntax errors.

--- docker/client/start.sh
+++ docker/client/start.sh
@@ -5,4 +5,10 @@ service shibd restart
 
 rm -f /run/apache2/apache2.pid
 apache2ctl -D FOREGROUND &
-yarn install && yarn dev --host ${CLIENT_HOST} --port ${SP_PORT}
+yarn install
+if [[ "$CLIENT_MODE" == "development" ]]; then
+  yarn dev --host ${CLIENT_HOST} --port ${SP_PORT}
+else
+  yarn build
+  yarn serve --host ${CLIENT_HOST} --port ${SP_PORT}
+fi
