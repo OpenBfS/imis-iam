72e454bc568d3ef41e2d2b69bbfa3709cabecf15
Christoph Klassen <christoph.klassen@intevation.de>
2025-01-16 17:49:40 +0100

    Add validation to export dialog

--- client/src/components/UI/ExportDialog.vue
+++ client/src/components/UI/ExportDialog.vue
@@ -12,48 +12,51 @@
     <v-container class="mt-4">
       <v-row justify="center">
         <v-col cols="10">
-          <v-combobox
-            :no-data-text="$t('label.no_data_text')"
-            :label="$t('label.field_seperator')"
-            :items="fieldSeparators"
-            v-model="csvOptions.fieldSeparator"
-            item-title="name"
-            item-value="value"
-          >
-          </v-combobox>
-          <v-combobox
-            :no-data-text="$t('label.no_data_text')"
-            :label="$t('label.row_delimiter')"
-            :items="rowDelimiters"
-            item-title="name"
-            item-value="value"
-            v-model="csvOptions.rowDelimiter"
-            persistent-hint
-          >
-          </v-combobox>
-          <v-combobox
-            :no-data-text="$t('label.no_data_text')"
-            :label="$t('label.encoding')"
-            :items="encoding"
-            item-title="name"
-            item-value="value"
-            v-model="csvOptions.encoding"
-            persistent-hint
-          >
-          </v-combobox>
-          <v-select
-            :no-data-text="$t('label.no_data_text')"
-            :label="$t('label.quote_type')"
-            :items="quoteTypes"
-            item-title="name"
-            item-value="value"
-            v-model="csvOptions.quoteType"
-            persistent-hint
-          >
-          </v-select>
+          <v-form v-model="valid" ref="form">
+            <Combobox
+              :label="$t('label.field_seperator')"
+              :items="fieldSeparators"
+              attribute="fieldSeparator"
+              item-title="name"
+              item-value="value"
+              required
+              @update:modelValue="csvOptions.fieldSeparator = $event"
+            ></Combobox>
+            <Combobox
+              :label="$t('label.row_delimiter')"
+              :items="rowDelimiters"
+              attribute="rowDelimiter"
+              item-title="name"
+              item-value="value"
+              persistent-hint
+              required
+              @update:modelValue="csvOptions.rowDelimiter = $event"
+            ></Combobox>
+            <Combobox
+              :label="$t('label.encoding')"
+              :items="encoding"
+              item-title="name"
+              item-value="value"
+              attribute="encoding"
+              persistent-hint
+              required
+              @update:modelValue="csvOptions.encoding = $event"
+            ></Combobox>
+            <Combobox
+              attribute="quoteType"
+              :no-data-text="$t('label.no_data_text')"
+              :label="$t('label.quote_type')"
+              :items="quoteTypes"
+              item-title="name"
+              item-value="value"
+              persistent-hint
+              required
+              @update:modelValue="csvOptions.quoteType = $event"
+            ></Combobox>
+          </v-form>
         </v-col>
         <UIAlert
-          v-if="hasRequestError"
+          v-if="hasRequestError || hasRequestError"
           v-bind:isSuccessful="!hasRequestError"
           v-bind:message="applicationStore.httpErrorMessage"
         />
@@ -62,7 +65,7 @@
     <v-divider></v-divider>
     <v-card-actions>
       <v-spacer></v-spacer>
-      <v-btn color="accent" @click="exportFile">
+      <v-btn color="accent" @click="exportFile" :disabled="!valid">
         {{ $t("label.export") }}
       </v-btn>
       <v-btn
@@ -76,13 +79,15 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
+import { onBeforeMount, onMounted, onUnmounted, ref } from "vue";
 import { useI18n } from "vue-i18n";
 import { useApplicationStore } from "@/stores/application.js";
 import { useUserStore } from "@/stores/user.js";
 import { useInstitutionStore } from "@/stores/institution.js";
 import { useNotification } from "@/lib/use-notification.js";
+import { useForm } from "@/lib/use-form.js";
 import { createSearchQueryString, HTTP, paramsSerializer } from "@/lib/http.js";
+import Combobox from "../Form/Combobox.vue";
 
 const applicationStore = useApplicationStore();
 const institutionStore = useInstitutionStore();
@@ -130,10 +135,41 @@ const rowDelimiters = [
 ];
 const encoding = ["iso-8859-15", "utf-16", "utf-8", "ascii"];
 const csvOptions = ref({
-  fieldSeparator: fieldSeparators[0],
-  rowDelimiter: rowDelimiters[0],
+  fieldSeparator: fieldSeparators[0].value,
+  rowDelimiter: rowDelimiters[0].value,
   encoding: encoding[0],
-  quoteType: quoteTypes[0],
+  quoteType: quoteTypes[0].value,
+});
+const { form, valid, reqField, validLength } = useForm();
+
+onBeforeMount(() => {
+  applicationStore.setForm(form);
+  applicationStore.initClientRules({
+    encoding: [...reqField(t("export.required_field_encoding"))],
+    fieldSeparator: [
+      ...reqField(t("export.required_field_seperator")),
+      ...validLength(
+        undefined,
+        1,
+        t("error.invalid_length_too_long", {
+          attribute: t("label.field_seperator"),
+          max: 1,
+        })
+      ),
+    ],
+    rowDelimiter: [...reqField(t("export.required_row_delimiter"))],
+    quoteType: [
+      ...reqField(t("export.required_quote_type")),
+      ...validLength(
+        undefined,
+        1,
+        t("error.invalid_length_too_long", {
+          attribute: t("label.quote_type"),
+          max: 1,
+        })
+      ),
+    ],
+  });
 });
 
 onMounted(() => {
@@ -156,6 +192,11 @@ onMounted(() => {
       institutionStore.filterBy
     );
   }
+  applicationStore.managedItem = csvOptions.value;
+});
+
+onUnmounted(() => {
+  applicationStore.removeAllResetEventListeners();
 });
 
 const exportRequest = (itemsName) => {
@@ -164,7 +205,7 @@ const exportRequest = (itemsName) => {
   const keys = Object.keys(options);
   for (const key of keys) {
     if (typeof options[key] === "object") {
-      params[key] = options[key].value;
+      params[key] = options[key]?.value;
     } else {
       params[key] = options[key];
     }
