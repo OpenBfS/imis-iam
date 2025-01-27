2fc623d47f2d9f9917859bf2984ccdc11c5ef526
Tom Gottfried <tom@intevation.de>
2025-01-27 15:37:52 +0100

    Simplify setup of example data at Keycloak startup

--- docker/keycloak/start.sh
+++ docker/keycloak/start.sh
@@ -8,124 +8,14 @@ then
 fi
 export PGPASSWORD=secret
 
+# Environment variables used in import file
+export SP_URL="${PROTOCOLL}://${SP_HOSTNAME}:${SP_PORT}"
+# Keycloak expects User Profile config as string
+export USER_PROFILE="$(jq -c '.' $DIR/user_profile.json | sed 's/\\/\\\\/g;s/\"/\\\"/g')"
+
 # Start Keycloak
-${KEYCLOAK_HOME}/bin/kc.sh --verbose start --optimized &
+${KEYCLOAK_HOME}/bin/kc.sh --verbose start --optimized --import-realm &
 wait-for-it localhost:8080 -t 120 || { echo "Failed starting Keycloak"; exit 1; }
-
-echo "Configure realm, SAML client and example account if not existing ..."
-KEYCLOAK_URL=http://localhost:8080
-KEYCLOAK_REALM=master
-IMIS_CLIENT=iam-client
-IMIS_CLIENT_ID=$(jq .id ${DIR}/iam_client.json | tr -d '"')
-
-# Try to get access token or wait until the initial admin user was added
-counter=1
-max_retry=10
-TKN="null"
-while [ "$TKN" = "null" -a "$counter" != "$max_retry" ]
-do
-    echo "Trying to get acess token..."
-    TKN=$(curl -sX POST "${KEYCLOAK_URL}/realms/${KEYCLOAK_REALM}/protocol/openid-connect/token" \
-    -H "Content-Type: application/x-www-form-urlencoded" \
-    -d "username=${KC_BOOTSTRAP_ADMIN_USERNAME}" \
-    -d "password=${KC_BOOTSTRAP_ADMIN_PASSWORD}" \
-    -d 'grant_type=password' \
-    -d 'client_id=admin-cli' | jq -r '.access_token')
-    counter=$((counter+1))
-    sleep 2
-done
-
-# Import imis3 realm
-REALM_CONFIG=$(sed "s/%IMIS_REALM/${IMIS_REALM}/g" ${DIR}/realm_config.json)
-echo "Creating imis realm '$IMIS_REALM'"
-curl -sX POST "${KEYCLOAK_URL}/admin/realms" \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Bearer $TKN" \
-    --data "$REALM_CONFIG"
-
-# Create SAML client for the client web application
-echo "Creating iam client: $IMIS_REALM->$(jq .clientId ${DIR}/iam_client.json)"
-SP_URL="${PROTOCOLL}://${SP_HOSTNAME}:${SP_PORT}"
-CLIENT_CONFIG=$(sed "s@%SP_URL@${SP_URL}@g" ${DIR}/iam_client.json)
-curl -sX POST "${KEYCLOAK_URL}/admin/realms/$IMIS_REALM/clients" \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Bearer $TKN" \
-    --data "$CLIENT_CONFIG"
-
-# Configure and sync LDAP
-echo "Creating LDAP user federation"
-curl -sX POST "${KEYCLOAK_URL}/admin/realms/$IMIS_REALM/components" \
-     -H "Content-Type: application/json" \
-     -H "Authorization: Bearer $TKN" \
-     -d @${DIR}/ldap_provider.json
-
-${KEYCLOAK_HOME}/bin/kcadm.sh config credentials \
-    --server http://localhost:8080 --realm master \
-    --user $KC_BOOTSTRAP_ADMIN_USERNAME --password $KC_BOOTSTRAP_ADMIN_PASSWORD
-
-echo "Creating user profile"
-${KEYCLOAK_HOME}/bin/kcadm.sh update realms/$IMIS_REALM/users/profile \
-    -f ${DIR}/user_profile.json
-
-echo "Creating example users"
-for user in "iam_user.json" "iam_redakteur.json" "iam_chefredakteur.json"
-do
-    ${KEYCLOAK_HOME}/bin/kcadm.sh create users -r $IMIS_REALM -f ${DIR}/${user}
-done
-
-echo "Creating client roles"
-for name in "chief_editor" "editor" "user"
-do
-    # Create roles with name and key for localization
-    role_id=$(${KEYCLOAK_HOME}/bin/kcadm.sh create \
-        clients/$IMIS_CLIENT_ID/roles -r "$IMIS_REALM" \
-            -s name="$name" -s description="role_iam_$name" -o | \
-        jq ".id" | tr -d '"')
-    if [ "$comp_role_id" ]; then
-        # Make previously created role a composite role by adding the new one
-        ${KEYCLOAK_HOME}/bin/kcadm.sh add-roles \
-            -r "$IMIS_REALM" --cclientid "$IMIS_CLIENT" \
-            --rid "$comp_role_id" \
-            --rolename "$name"
-    fi
-    comp_role_id="$role_id"
-done
-
-roles=$(curl -sX GET "${KEYCLOAK_URL}/admin/realms/$IMIS_REALM/clients/$IMIS_CLIENT_ID/roles" \
-        -H "Content-Type: application/json" \
-        -H "Authorization: Bearer $TKN")
-users=$(curl -sX GET "${KEYCLOAK_URL}/admin/realms/$IMIS_REALM/users" \
-        -H "Content-Type: application/json" \
-        -H "Authorization: Bearer $TKN")
-
-echo "Assigning groups and client roles"
-
-#Assign user role
-userRole=$(echo $roles | jq 'map(select(.name=="user"))')
-userId=$(echo $users | jq 'map(select(.username=="exampleuser")) | .[0]' | jq .id | tr -d '"')
-
-curl -sX POST "${KEYCLOAK_URL}/admin/realms/$IMIS_REALM/users/$userId/role-mappings/clients/$IMIS_CLIENT_ID" \
-        -H "Content-Type: application/json" \
-        -H "Authorization: Bearer $TKN" \
-        -d "$userRole"
-
-#Assign editor role
-editorRole=$(echo $roles | jq 'map(select(.name=="editor"))')
-editorUserId=$(echo $users | jq 'map(select(.username=="redakteur")) | .[0]' | jq .id | tr -d '"')
-
-curl -sX POST "${KEYCLOAK_URL}/admin/realms/$IMIS_REALM/users/$editorUserId/role-mappings/clients/$IMIS_CLIENT_ID" \
-        -H "Content-Type: application/json" \
-        -H "Authorization: Bearer $TKN" \
-        -d "$editorRole"
-
-#Assign chief_editor role
-chiefEditorRole=$(echo $roles | jq 'map(select(.name=="chief_editor"))')
-chiefEditorUserId=$(echo $users | jq 'map(select(.username=="chefredakteur")) | .[0]' | jq .id | tr -d '"')
-
-curl -sX POST "${KEYCLOAK_URL}/admin/realms/$IMIS_REALM/users/$chiefEditorUserId/role-mappings/clients/$IMIS_CLIENT_ID" \
-        -H "Content-Type: application/json" \
-        -H "Authorization: Bearer $TKN" \
-        -d "$chiefEditorRole"
 
 # Add example data
 psql -h db -U keycloak -d keycloak -a -f ${DIR}/add_example_data.sql -w
