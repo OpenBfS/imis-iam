7215752082b53fd5b0b2ab328e0ed90bf2dd2f58
Christoph Klassen <christoph.klassen@intevation.de>
2025-02-03 21:14:14 +0100

    Migrate to eslint 9

---
+++ client/eslint.config.js
@@ -0,0 +1,52 @@
+/* Copyright (C) 2022 by Bundesamt fuer Strahlenschutz
+ * Software engineering by Intevation GmbH
+ *
+ * This file is Free Software under the GNU GPL (v>=3)
+ * and comes with ABSOLUTELY NO WARRANTY!
+ */
+import js from "@eslint/js";
+import pluginVue from "eslint-plugin-vue";
+import prettier from "eslint-config-prettier";
+import notice from "eslint-plugin-notice";
+
+export default [
+  {
+    name: "app/files-to-lint",
+    files: ["**/*.{js,mjs,jsx,vue}"],
+  },
+
+  {
+    name: "app/files-to-ignore",
+    ignores: ["**/dist/**", "**/dist-ssr/**", "**/coverage/**", ".gitignore"],
+  },
+
+  js.configs.recommended,
+  ...pluginVue.configs["flat/essential"],
+  {
+    rules: {
+      "vue/script-setup-uses-vars": "error",
+      "vue/multi-word-component-names": "off",
+      "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+      "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    },
+  },
+  {
+    name: "license-check",
+    files: ["src/components/*.vue", "src/components/*/*.vue", "src/*.vue"],
+    plugins: {
+      notice,
+    },
+    rules: {
+      "notice/notice": [
+        "warn",
+        {
+          templateFile: "./src/license/vueTemplate.txt",
+          messages: {
+            whenFailedToMatch: "Missing or wrong license header",
+          },
+        },
+      ],
+    },
+  },
+  prettier,
+];
