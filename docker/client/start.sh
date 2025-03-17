6386bcb36738159025fcf01ee6e68aaac22c400e
Tom Gottfried <tom@intevation.de>
2025-03-17 17:42:31 +0100

    Drop configuration variable
    Unnecessary to make this configurable in the context of this setup.

--- docker/client/start.sh
+++ docker/client/start.sh
@@ -7,8 +7,8 @@ rm -f /run/apache2/apache2.pid
 apache2ctl -D FOREGROUND &
 yarn install
 if [[ "$CLIENT_MODE" == "development" ]]; then
-  yarn dev --host ${CLIENT_HOST} --port ${PORT} --base ${CLIENT_PATH}
+  yarn dev --host 0.0.0.0 --port ${PORT} --base ${CLIENT_PATH}
 else
   yarn build --base ${CLIENT_PATH}
-  yarn serve --host ${CLIENT_HOST} --port ${PORT} --base ${CLIENT_PATH}
+  yarn serve --host 0.0.0.0 --port ${PORT} --base ${CLIENT_PATH}
 fi
