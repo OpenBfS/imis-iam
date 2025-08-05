06adf3785153b86b913e3d25e350cb0b02b5e352
Sebastian Wagner <swagner@intevation.de>
2025-08-05 16:08:07 +0200

    6017: client docker: httpd im Vordergrund
    Nutze das httpd image und installiere openidc dazu
    Nutze httpd-foreground als entrypoint, sodass der container schneller
    auf stop Signale reagiert

--- docker/client/start.sh
+++ docker/client/start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+set -eu -o pipefail
+
 rm -f /run/apache2/apache2.pid
 
-if [[ "$CLIENT_MODE" == "development" ]]; then
-    a2ensite -q dev
+if [[ "${CLIENT_MODE:-production}" == "development" ]]; then
+    sed -i "$ a Include /usr/local/iamdocker/dev.conf" /usr/local/apache2/conf/httpd.conf
     # Install in case a clean checkout is bind-mounted in dev setup
     yarn install && yarn dev --host 0.0.0.0 --port 8080 --base ${CLIENT_PATH} &
 fi
 
-apache2ctl -D FOREGROUND
+exec httpd-foreground
