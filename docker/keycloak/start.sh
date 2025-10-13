4319b7305e0e687d2282b26310823f8f832ea29e
Frank Fuhlbrück, Bundesamt fuer Strahlenschutz, RN 1 <ffuhlbrueck@bfs.de>
2025-10-13 14:15:29 +0200

    keycloak start.sh: make start time diff computation more robust against file system errors

--- docker/keycloak/start.sh
+++ docker/keycloak/start.sh
@@ -4,17 +4,20 @@ if [ ! -f ${KEYCLOAK_HOME}/nstarts ] ; then echo 0 > ${KEYCLOAK_HOME}/nstarts; f
 nstarts=$(($(cat ${KEYCLOAK_HOME}/nstarts) + 1))
 echo $nstarts  > ${KEYCLOAK_HOME}/nstarts
 if [ ! -f ${KEYCLOAK_HOME}/laststart ] ; then echo 0 > ${KEYCLOAK_HOME}/laststart; fi
-if [ $(($(date +%s) - $(cat ${KEYCLOAK_HOME}/laststart))) -le  120 ] ; then
-  echo "start $nstarts: last start was less than 2 minutes ago - waiting for 3 minutes"
+laststart=$(cat ${KEYCLOAK_HOME}/laststart)
+laststartform=$(echo $laststart | sed 's/^[0-9]*$/FORMOK/')
+if [ "$laststartform" != "FORMOK" ]; then laststart=$(date +%s); fi
+
+if [ $(($(date +%s) - $laststart)) -le  120 ] ; then
+  echo "start $nstarts ($(date)): last start was less than 2 minutes ago (at $(date -d @$laststart)) - waiting for 3 minutes"
   echo $(date +%s) > ${KEYCLOAK_HOME}/laststart
   sleep 180
   echo "waiting finished - service should start now"
   #next start after a long start is also always fast
 else
-  echo "start $nstarts: last start was more than 2 minutes ago - no delay"
+  echo "start $nstarts ($(date)): last start was more than 2 minutes ago (at $(date -d @$laststart)) - no delay"
   echo $(date +%s) > ${KEYCLOAK_HOME}/laststart
 fi
-
 
 # Start Keycloak via exec to override the bash process:
 exec ${KEYCLOAK_HOME}/bin/kc.sh start --optimized
