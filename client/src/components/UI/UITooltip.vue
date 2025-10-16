3bf9c4e9db4818a3f6b1eb35eec35b424517298b
Christoph Klassen <christoph.klassen@intevation.de>
2025-10-16 15:29:35 +0200

    Create custom tooltip component
    That way we ensure that tooltips are even shown for disabled
    buttons.
    
    The span-wrapper is necessary because usually tooltops are not
    displayed for disabled buttons.

---
+++ client/src/components/UI/UITooltip.vue
@@ -0,0 +1,27 @@
+<!--
+ Copyright (C) 2025 by Bundesamt fuer Strahlenschutz
+ Software engineering by Intevation GmbH
+
+ This file is Free Software under the GNU GPL (v>=3)
+ and comes with ABSOLUTELY NO WARRANTY!
+ -->
+<template>
+  <v-tooltip :location="props.location ?? 'top'">
+    <template v-slot:activator="{ props }">
+      <span v-bind="props">
+        <slot></slot>
+      </span>
+    </template>
+    <span>{{ props.tooltipText }}</span>
+  </v-tooltip>
+</template>
+
+<script setup>
+const props = defineProps([
+  // Vuetify props
+  "location",
+
+  // Custom props
+  "tooltipText",
+]);
+</script>
