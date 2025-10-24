567544e84e3d7edf42a1e541b438010019206b43
Christoph Klassen <christoph.klassen@intevation.de>
2025-10-24 11:42:56 +0200

    Only send request when filter field loses focus
    This prevents that a request is sent every time the user (de-)selects a value
    in the list. This is necessary because the filter operation is costly.

--- client/src/components/Search/Filter.vue
+++ client/src/components/Search/Filter.vue
@@ -26,10 +26,14 @@
         !['enabled', 'hiddenInAddressbook', 'role'].includes(filterKey)
       "
       variant="outlined"
+      @update:focused="
+        (event) => {
+          if (!event) handleFilterInput();
+        }
+      "
       @update:modelValue="
         (event) => {
           autocompleteValue = event;
-          handleFilterInput(event);
         }
       "
     ></v-autocomplete>
@@ -109,7 +113,8 @@ const triggerSearch = debounce(() => {
   applicationStore.searchRequest([props.type]);
 }, 500);
 
-const handleFilterInput = (value) => {
+const handleFilterInput = () => {
+  const value = autocompleteValue.value;
   const term = value !== null ? value : "";
   if (props.type === "users") {
     userStore.updateFilter(props.filterKey, term);
